6c6bb4ff4a1be6d6399e7265fccbcc229ee5d572
Sinan Şenkul <101463814+SinanSenkul@users.noreply.github.com>
2024-08-28 02:39:45 +0300

    Add files via upload

---
+++ Ödev 10.sql
@@ -0,0 +1,20 @@
+-- query 1
+
+SELECT city.city, country.country
+FROM city
+LEFT JOIN country
+ON city.country_id = country.country_id;
+
+-- query 2
+
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+RIGHT JOIN customer
+ON payment.customer_id = customer.customer_id;
+
+-- query 3
+
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+FULL JOIN customer
+ON customer.customer_id = rental.customer_id;
